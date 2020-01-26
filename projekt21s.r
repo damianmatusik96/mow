ea8504c2db8e13e416ba61e52246976ddfb5dbe9
Your Name <siowszek.m@gmail.com>
2020-01-26 15:28:09 +0100

    dodanie cheestow

--- projekt21s.r
+++ projekt21s.r
@@ -33,7 +33,6 @@ data <- prepare_data(wine_quality)
 # data <- prepare_data(zmienioneDane)
 # blad <- 0
 
-# Modele globalne na calym zbiorze trenujacym
 global_svm_model <- svm(formula = quality ~ ., data = data@train_data, type = 'C', kernel = "linear")
 global_tree_model <- rpart(quality~., method="class", data=data@train_data, control=rpart.control(minsplit=5))
 
@@ -42,7 +41,6 @@ svm_effectiveness <- 0
 global_svm_effectiveness <- 0
 tree_effectiveness <- 0
 global_tree_effectiveness <- 0
-gbm_effectiveness <- 0
 
 
 for(siema in 1:2) {
@@ -67,20 +65,20 @@ for(siema in 1:2) {
   gbm_result <- 0
   
   for (i in 1:nrow(id_neighbours)){
-    neighbours_data <- data.frame()
+    neighbours_data <- data@train_data[which(data@train_data$id == id_neighbours[i,1]),]
     true_quality[i] <- data@test_data_with_class[which(data@test_data_with_class$id == query[i,1]),]$quality
     
-      for (n in 1:ncol(id_neighbours)){
         id <- id_neighbours[i,n]
         neighbours_data <- rbind(neighbours_data,data@train_data[which(data@train_data$id == id),])
       }
-    knn_predicted_quality[i] <- neighbours$test_set_scores[i,1]
   
     #svm_model <- svm(formula = quality ~ ., data = neighbours_data, type = 'C', kernel = "linear")
     #summary(svm_model)
     #pred <- predict(svm_model,query[i,])
     svm_predicted_quality[i] <- svm_predict(neighbours_data,query[i,])
+    for (n in 2:ncol(id_neighbours)){
     
+    knn_predicted_quality[i] <- as.numeric(tail(names(sort(table(neighbours_data$quality))), 1))
     #global svm
     pred <- predict(global_svm_model,query[i,])
     global_svm_quality[i] <- as.numeric(levels(pred))[pred]
@@ -92,17 +90,10 @@ for(siema in 1:2) {
     pred <- predict(global_tree_model, query[i,], type="prob")
     global_tree_quality[i] <- as.numeric(colnames(pred)[apply(pred,1,which.max)])
     
-    gbm_fit <- gbm(formula = quality ~ ., 
-                   distribution = "gaussian", 
-                   data = neighbours_data[,2:ncol(neighbours_data)], 
-                   n.trees = 300, 
-                   interaction.depth = 4, 
-                   shrinkage = 0.01)
-    
-    gbm_result[i] <- round(predict(gbm_fit, query[i,], n.trees = 300))
+    #system.time(pred <- predict(svm_model,quality ~ .))
   }
   
-  result <- cbind(true_quality,knn_predicted_quality, svm_predicted_quality, global_svm_quality, tree_predicted_quality, global_tree_quality, gbm_result) 
+  result <- cbind(true_quality,knn_predicted_quality, svm_predicted_quality, global_svm_quality, tree_predicted_quality, global_tree_quality) 
   knn_effectiveness[siema] <- nrow(result[which(result[,1] == result[,2]),])
   
   svm_effectiveness[siema] <- nrow(result[which(result[,1] == result[,3]),])
